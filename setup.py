0aaf45d9e78e075436b704c9b2429317b18d8830
Tony Landis <tony.landis@gmail.com>
2013-11-09 15:10:13 -0600

    doc tweaks

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 
 setup(
   name='datomic',
-  version='0.1dev',
+  version='0.1.2dev',
   description='Interface to the Datomic REST API',
-  long_description=open('README.md').read(),
+  #long_description=open('README.md').read(),
   author='Tony Landis',
   author_email='tony.landis@gmail.com',
   license="Apache 2",
   url='https://github.com/tony-landis/datomic-py',
   install_requires=[
-    'edn_format'
-    'urllib3'
-    'termcolor'
+    'edn_format',
+    'urllib3',
+    'termcolor',
   ],
   packages = ['datomic', ],
 )
